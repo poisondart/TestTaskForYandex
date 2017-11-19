cb19cf49717e49e1e923e95915d89dda5a408e54
Nikita Karpunin <novembernix@gmail.com>
2017-11-19 16:50:49 +0300

    Update Constants.java

--- app/src/main/java/com/example/nix/testtaskforyandex/Constants.java
+++ app/src/main/java/com/example/nix/testtaskforyandex/Constants.java
@@ -5,6 +5,6 @@ package com.example.nix.testtaskforyandex;
  */
 
 public class Constants {
-    public static final String API_KEY = "***************"; /*получите свой ключ*/
+    public static final String API_KEY = "trnsl.1.1.20170315T182814Z.f2c6af06a3d2507e.ffa3788d89671b073c8fbd3493b0fdbc31405d6f";
     public static final String URL = "https://translate.yandex.net";
 }
